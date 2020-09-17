06fdf02d171c810f77155a0a8e2e65619e660dfe
AntonLPF <somemail@gmail.com>
2020-09-17 20:14:01 +0300

    Finished chapter 3.p139

--- Bullseye/ContentView.swift
+++ Bullseye/ContentView.swift
@@ -15,21 +15,24 @@ struct ContentView: View {
     
 //    User interface views
     @State var alertIsVisible: Bool = false
+    @State var sliderValue: Double = 50.0
     
 //    User interface content and layout
     var body: some View {
         VStack {
-            
+            Spacer()
 //            Target row
             HStack {
                 Text("Put the bullseye as close as you can to:")
                 Text("100")
             }
             
+            Spacer()
+            
 //            Slider row
             HStack{
                 Text("1")
-                Slider(value: .constant(10))
+                Slider(value: self.$sliderValue, in: 1...100)
                 Text("100")
             }
             
@@ -41,22 +44,28 @@ struct ContentView: View {
                 Text("Hit me!")
             }
             .alert(isPresented: self.$alertIsVisible) {
-                Alert(title: Text("Hello there!"), message: Text("This is my first pop-up"), dismissButton: .default(Text("Awesome")))
+                Alert(title: Text("Hello there!"), message: Text("The slider's value is \(Int(self.sliderValue.rounded()))."), dismissButton: .default(Text("Awesome")))
             }
+            
+            Spacer()
             
 //            Score row
             HStack {
                 Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
                     Text("Start over")
                 }
+                Spacer()
                 Text("Score")
                 Text("999999")
+                Spacer()
                 Text("Round:")
                 Text("999")
+                Spacer()
                 Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
                     Text("Info")
                 }
             }
+            .padding(.bottom, 20)
         }
     }
     
