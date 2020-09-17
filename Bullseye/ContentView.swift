7844023071e42fc6447cafba83fe50500bab11ab
AntonLPF <antonsiskin@iMac-Anton.local>
2020-09-17 16:33:43 +0300

    Initial commit

---
+++ Bullseye/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Bullseye
+//
+//  Created by Антон Шишкин on 17.09.2020.
+//  Copyright © 2020 Антон Шишкин. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
